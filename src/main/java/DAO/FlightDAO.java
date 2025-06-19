80ea100b376f9ca3e883d59a16c89201f74b6059
akaka111 <khoado1777@gmail.com>
2025-06-19 21:33:59 +0700

    Revert "Bảo stage1"

--- src/main/java/DAO/FlightDAO.java
+++ src/main/java/DAO/FlightDAO.java
@@ -17,8 +17,7 @@ public class FlightDAO extends DBContext {
 
     public List<Flight> getAllFlights() {
         List<Flight> flights = new ArrayList<>();
-        String sql = "SELECT f.*, a.airline_name FROM Flight f "
-                + "JOIN Airline a ON f.airline_id = a.airline_id";
+        String sql = "SELECT * FROM Flight;";
 
         try ( Connection conn = getConnection();  PreparedStatement ps = conn.prepareStatement(sql);  ResultSet rs = ps.executeQuery()) {
 
